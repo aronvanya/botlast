9050091f12cfbd9b3362e443e9ff0bacf4d190fc
aronvanya <aronvanya@gmail.com>
2025-01-20 20:16:20 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-@app.route("/webhook", methods=["POST"])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.headers.get("content-type") == "application/json":
+    if request.headers.get('content-type') == 'application/json':
         update = request.get_json()
-        # Здесь можно обрабатывать update
+        # Здесь обработка сообщений бота
         return {"status": "ok"}, 200
     else:
         return {"status": "Unsupported Media Type"}, 415
